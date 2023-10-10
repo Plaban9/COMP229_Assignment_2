95ede089dccfccd140517c9927a24d75fabe771f
Plaban Biswas <plabanbiswas96@outlook.com>
2023-10-10 17:17:47 -0400

    - ADDED: GET api for name parameter where it can contain part of string
    - Used Regex to determine that

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,15 +1,37 @@
 let Product = require('../models/product.model');
 
-module.exports.save = async (req, res) =>
-{
-    const product = new Product(req.body);
-    let result = await product.save();
-    res.json(result);
-}
-
 module.exports.find = async (req, res) =>
 {
-    console.log(req.params);
+    if (req.query.name)
+    {
+        // Get Product by name specified in GET parameter - URL: api/product/?name (contains - perhaps Regex?)
+        // DOC - regex: https://www.mongodb.com/docs/manual/reference/operator/query/regex/
+        // DOC - find(): https://mongoosejs.com/docs/api/model.html#Model.find()
+        try 
+        {
+            let result = await Product.find({ "name": { $regex: req.query.name, $options: 'i' } });
+            res.json(result);
+        }
+        catch (exception) 
+        {
+            console.log(exception);
+            var emptyJson = {};
+            res.json(emptyJson);
+        }
+    }
+    else
+    {
+        // Get all Products - URL: api/product/
+        let result = await Product.find(req.params);
+        res.json(result);
+    }
+}
+
+// Get Product by id - URL: api/product/:id
+module.exports.findById = async (req, res) =>
+{
+    // DOC - findById(): https://mongoosejs.com/docs/api/model.html#Model.findById()
+
     try 
     {
         let result = await Product.find({ "_id": req.params.id });
@@ -39,9 +61,9 @@ module.exports.update = async (req, res) =>
     }
 }
 
-module.exports.findAll = async (req, res) =>
+module.exports.save = async (req, res) =>
 {
-    console.log(req.params);
-    let result = await Product.find(req.params);
+    const product = new Product(req.body);
+    let result = await product.save();
     res.json(result);
 }
--- routes/product.js
+++ routes/product.js
@@ -2,12 +2,21 @@ var express = require('express');
 var router = express.Router();
 var productController = require('../controllers/product.controller');
 
+// Get Product by id - URL: api/product/?name
+// router.get('/name', productController.findWithName); 
+
 /* GET product listing. */
 router
-  .get('/', productController.findAll)
+  .get('/', productController.find) // Get all Products - URL: api/product/
   .post('/', productController.save);
 
-router.get('/:id', productController.find);
+// Get Product by id - URL: api/product/:id
+router.get('/:id', productController.findById); 
+
+
+
+// Get Product by name - URL: api/product/:id
 router.put('/:id', productController.update);
+
 
 module.exports = router;
